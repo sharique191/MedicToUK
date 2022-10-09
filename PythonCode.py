f5c9a1d65a7bc5a4213b2c15df5f4ef3ef59d4ae
Syed Mubashir Iftekhar <55195432+sharique191@users.noreply.github.com>
2022-10-09 18:51:50 +0530

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -1,39 +1,53 @@
-import docx
+# This code automates the process of rephrasing questionbooks
+
+import pandas as pd
 import requests
 import json
 
 def change(a):                        # this function uses API to reframe the question
-    key="a3********************d032b"
+    key="a*********" # Key has been masked for confidentiality
     email="drarunavadhar@gmail.com"
     inp=a
     p={"email":email,"key":key,"input":inp,"rewrite_num":1,"uniqueness":1,"return_rewrites":"true"}
     r=requests.post("https://wai.wordai.com/api/rewrite",params=p)
+    if r.status_code!=200:           # returning False for error rows
+        return False
     r1=r.json()
     r2=r1["rewrites"]
     r2=r2[0]
     r2=r2.split('\n')
     r2=r2[0]
-    return r2
-    
+    return r2                          # function returns the rephrased string
 
-d=docx.Document('Original.docx')        # importing the original question-bank
-p=d.paragraphs                         # separating the paragraphs         
+a=pd.read_csv("1700Subjectwise 100 Rows - Sheet1.csv")  # a is pandas dataframe that loads original question set
+c=a.copy()                                             # c is a copy of a.      We modify in c and do not change a
+e=a.copy()                                              # e is also a copy of c meant to store error rows
 
-for i in p:                            # removing extra paragraphs or empty paragraphs
-    if i.text==None or i.text=="\n" or i.text=='' or i.text==" ":
-        p.remove(i)
+l=len(c)
+for i in range(0,l-1):
+    e=e.drop(i)                     # drop every row in e except the first row to keep column names intact
+
+z=0                                # maintaining z to append rows in e
+
+                                                # l is No of columns in a
+for i in range(0,l):                                    # we run this loop for l no of columns ie all columns
+    k=c["Question"][i]                                         # Extracting Question from Column named "H" and storing in variable k 
+    chng=change(k)
+    if chng==False:
+        e.loc[z]=c.loc[i]          # appending row in e 
+        z+=1
+        c["Question"][i]="*****Error*********"
+    else:
+        c["Question"][i]=chng                                 # Rephrasing the Question and saving it
         
-d2=docx.Document()                      # creating new blank file                          
-
-for i in p:                             # copying everything from original file to new file
-    d2.add_paragraph(i.text)
-
-p2=d2.paragraphs
-
-a=p[2].text                            # extracting the question
-p2[2].text=change(a)                   # reframing the question and puting that in new file    
-
-a=p[12].text                           ## Note: the index 2,12 has to be decided based on number of options
-p2[12].text=change(a)
-
-d2.save("RePhrased.docx")               # Exporting and Saving the Reframed Question-Bank
+e.reset_index(drop=True,inplace=True)  # reseting indexes of e  
+c.reset_index(drop=True,inplace=True)  # reseting indexes of c
+e=e.drop(0)                            # droping first row from e
+e.reset_index(drop=True,inplace=True)
+    
+c.to_csv(r'C:\Users\Syed Mubashir\Desktop\export_dataframe.csv', index = False, header=True)
+e.to_csv(r'C:\Users\Syed Mubashir\Desktop\error_dataframe.csv', index = False, header=True)
+         # The above line exports the panda frame as .csv file in the given location
+         # Kindly modify the path where you want to save the rephrased .csv file
+         # Also you may change the file name. Here the file name is "export_dataframe.csv"
+         # eg c.to_csv(r'Path\FileName.csv', index = False, header=True)
